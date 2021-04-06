95a22c45b6fa1011fa1b979f24b2f01312616e40
dev-stan <mrstanleyzalewski@gmail.com>
2021-04-06 18:59:45 +0200

    Create ChatListener file

---
+++ src/dev/stan/plugin/listeners/ChatListener.java
@@ -0,0 +1,37 @@
+package dev.stan.plugin.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import dev.stan.plugin.RickChat;
+
+// Permissions:
+//   mute: "rick.mute"
+
+public class ChatListener implements Listener {
+	
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event) {
+		
+		Player player = event.getPlayer();
+		
+		if (!player.hasPermission("rick.mute")) {
+			
+			if (event.getMessage().startsWith("rick") || (event.getMessage().startsWith("Rick"))) {
+				
+				if (event.getMessage().contains("help?")) {
+					
+					player.sendMessage(new RickChat().getConfig().getString("help"));
+				}
+				
+				if (event.getMessage().contains("rules?")) {
+					
+					player.sendMessage(new RickChat().getConfig().getString("rules"));
+				}
+
+			}
+		}
+	}
+}
